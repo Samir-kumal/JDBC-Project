a00ca1e70e42a987de3fbe0332ab3008c7c536a9
Samir-Kumal <kumalsameer124@gmai.com>
2023-05-16 22:30:12 +0545

    added student class

---
+++ project-two/src/Student.java
@@ -0,0 +1,59 @@
+public class Student {
+   private int id,age;
+   private String name, email, phoneNo,faculty;
+
+    Student(int id, String name, String email, String phoneNo, String faculty){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.faculty = faculty;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+}
